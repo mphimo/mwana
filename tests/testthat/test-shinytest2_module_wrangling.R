ec501a4eb0223564a1b613718cac65965acc3d7c
tomaszaba <tomas.zaba@outlook.com>
2025-10-30 18:06:35 +0430

    fix test failing on macos only; put back windowns-latest

--- tests/testthat/test-shinytest2_module_wrangling.R
+++ tests/testthat/test-shinytest2_module_wrangling.R
@@ -6,33 +6,32 @@
 ## ---- Data Wrangling: WFHZ ---------------------------------------------------
 
 testthat::test_that(desc = "Server data wrangling works as expected for WFHZ", {
-
   ## Initialise app ----
   app <- shinytest2::AppDriver$new(
     app_dir = testthat::test_path("fixtures"),
-      load_timeout = 120000, 
-      wait = TRUE
+    load_timeout = 120000,
+    wait = TRUE
   )
 
   ### Let the app load ----
-    app$wait_for_idle(timeout = 40000)
+  app$wait_for_idle(timeout = 40000)
 
-    ### Click on the Data uploading navbar ----
-    app$click(selector = "a[data-value='Data Upload']")
-    app$wait_for_idle(timeout = 40000)
+  ### Click on the Data uploading navbar ----
+  app$click(selector = "a[data-value='Data Upload']")
+  app$wait_for_idle(timeout = 40000)
 
-    ### Upload data ----
-    #### Read data ----
-    data <- read.csv(
-      file = system.file("app", "anthro-01.csv", package = "mwana"),
-      check.names = FALSE
-    )
-    tempfile <- tempfile(fileext = ".csv")
-    write.csv(data, tempfile, row.names = FALSE)
+  ### Upload data ----
+  #### Read data ----
+  data <- read.csv(
+    file = system.file("app", "anthro-01.csv", package = "mwana"),
+    check.names = FALSE
+  )
+  tempfile <- tempfile(fileext = ".csv")
+  write.csv(data, tempfile, row.names = FALSE)
 
-    #### Upload onto the app ----
-    app$upload_file(`upload_data-upload` = tempfile, wait_ = TRUE)
-  
+  #### Upload onto the app ----
+  app$upload_file(`upload_data-upload` = tempfile, wait_ = TRUE)
+
   ### Click on the data wrangling tab ----
   app$click(selector = "a[data-value='Data Wrangling']")
   app$wait_for_idle(timeout = 40000)
@@ -58,48 +57,46 @@ testthat::test_that(desc = "Server data wrangling works as expected for WFHZ", {
   ") |> as.character()
 
   testthat::expect_true(all(c("wfhz", "flag_wfhz") %in% vals))
-   testthat::expect_equal(
+  testthat::expect_equal(
     object = length(vals),
-    expected = 14)
+    expected = 14
+  )
 
   #### Stop the app ----
   app$stop()
-}
-
-)
+})
 
 
 ## ---- Data Wrangling: MFAZ ---------------------------------------------------
 
 
 testthat::test_that(desc = "Server data wrangling works as expected for MFAZ", {
-
   ## Initialise app ----
   app <- shinytest2::AppDriver$new(
     app_dir = testthat::test_path("fixtures"),
-      load_timeout = 120000,
-      wait = TRUE
+    load_timeout = 120000,
+    wait = TRUE
   )
 
   ### Let the app load ----
-    app$wait_for_idle(timeout = 40000)
+  app$wait_for_idle(timeout = 40000)
 
-    ### Click on the Data uploading navbar ----
-    app$click(selector = "a[data-value='Data Upload']")
-    app$wait_for_idle(timeout = 40000)
+  ### Click on the Data uploading navbar ----
+  app$click(selector = "a[data-value='Data Upload']")
+  app$wait_for_idle(timeout = 40000)
 
-    ### Upload data ----
-    #### Read data ----
-    data <- read.csv(
-      file = system.file("app", "anthro-01.csv", package = "mwana"),
-      check.names = FALSE
-    )
-    tempfile <- tempfile(fileext = ".csv")
-    write.csv(data, tempfile, row.names = FALSE)
+  ### Upload data ----
+  #### Read data ----
+  data <- read.csv(
+    file = system.file("app", "anthro-01.csv", package = "mwana"),
+    check.names = FALSE
+  )
+  tempfile <- tempfile(fileext = ".csv")
+  write.csv(data, tempfile, row.names = FALSE)
 
-    #### Upload onto the app ----
-    app$upload_file(`upload_data-upload` = tempfile, wait_ = TRUE)
-  
+  #### Upload onto the app ----
+  app$upload_file(`upload_data-upload` = tempfile, wait_ = TRUE)
+
   ### Click on the data wrangling tab ----
   app$click(selector = "a[data-value='Data Wrangling']")
   app$wait_for_idle(timeout = 40000)
@@ -127,13 +124,11 @@ testthat::test_that(desc = "Server data wrangling works as expected for MFAZ", {
 
   ### Test check ----
   testthat::expect_true(all(vals %in% c("age_days", "mfaz", "flag_mfaz")))
-    testthat::expect_true(app$get_js("$('#ipc_check-checked').length > 0"))
+  testthat::expect_true(app$get_js("$('#ipc_check-checked').length > 0"))
 
   #### Stop the app ----
   app$stop()
-}
-
-)
+})
 
 
 ## ---- Data Wrangling: MUAC ---------------------------------------------------
@@ -142,8 +137,6 @@ testthat::test_that(desc = "Server data wrangling works as expected for MFAZ", {
 testthat::test_that(
   desc = "Prevalence tab works as expected when age is given in categories",
   code = {
-
-
     #### Initialise app ----
     app <- shinytest2::AppDriver$new(
       app_dir = testthat::test_path("fixtures"),
@@ -187,19 +180,18 @@ testthat::test_that(
     app$wait_for_idle(timeout = 40000)
 
     ### Get wrangled values ----
-  vals <- app$get_js("
+    vals <- app$get_js("
     $('#wrangle_data-wrangled thead th').map(function() {
       return $(this).text();
     }).get();
   ") |> as.character()
 
-  ### Test check ----
-  testthat::expect_true("flag_muac" %in% vals)
-        testthat::expect_true(app$get_js("$('#ipc_check-checked').length > 0"))
+    ### Test check ----
+    testthat::expect_true("flag_muac" %in% vals)
+    testthat::expect_true(app$get_js("$('#ipc_check-checked').length > 0"))
 
-  ### Stop the app ----
+    ### Stop the app ----
     app$stop()
-
   }
 )
 
@@ -207,16 +199,16 @@ testthat::test_that(
 ## ---- Data Wrangling: WFHZ and MFAZ ------------------------------------------
 
 testthat::test_that(
-  desc = "Server data wrangling works as expected for combined wrangling", {
-
-  ## Initialise app ----
-  app <- shinytest2::AppDriver$new(
-    app_dir = testthat::test_path("fixtures"),
+  desc = "Server data wrangling works as expected for combined wrangling",
+  {
+    ## Initialise app ----
+    app <- shinytest2::AppDriver$new(
+      app_dir = testthat::test_path("fixtures"),
       load_timeout = 120000,
       wait = TRUE
-  )
+    )
 
-  ### Let the app load ----
+    ### Let the app load ----
     app$wait_for_idle(timeout = 40000)
 
     ### Click on the Data uploading navbar ----
@@ -234,42 +226,30 @@ testthat::test_that(
 
     #### Upload onto the app ----
     app$upload_file(`upload_data-upload` = tempfile, wait_ = TRUE)
-  
-  ### Click on the data wrangling tab ----
-  app$click(selector = "a[data-value='Data Wrangling']")
-  app$wait_for_idle(timeout = 40000)
 
-  ### Select input variables ----
-  app$set_inputs(`wrangle_data-wrangle` = "combined", wait_ = FALSE)
-  app$set_inputs(`wrangle_data-dos` = "", wait_ = FALSE)
-  app$set_inputs(`wrangle_data-dob` = "", wait_ = FALSE)
-  app$set_inputs(`wrangle_data-age` = "age", wait_ = FALSE)
-  app$set_inputs(`wrangle_data-sex` = "sex", wait_ = FALSE)
-  app$set_inputs(`wrangle_data-weight` = "weight", wait_ = FALSE)
-  app$set_inputs(`wrangle_data-height` = "height", wait_ = FALSE)
-  app$set_inputs(`wrangle_data-muac` = "muac", wait_ = FALSE)
+    ### Click on the data wrangling tab ----
+    app$click(selector = "a[data-value='Data Wrangling']")
+    app$wait_for_idle(timeout = 40000)
 
-  ### Click wrangle button ----
-  app$click(input = "wrangle_data-apply_wrangle")
-  app$wait_for_value(output = "wrangle_data-wrangled", timeout = 40000)
+    ### Select input variables ----
+    app$set_inputs(`wrangle_data-wrangle` = "combined", wait_ = FALSE)
+    app$set_inputs(`wrangle_data-dos` = "", wait_ = FALSE)
+    app$set_inputs(`wrangle_data-dob` = "", wait_ = FALSE)
+    app$set_inputs(`wrangle_data-age` = "age", wait_ = FALSE)
+    app$set_inputs(`wrangle_data-sex` = "sex", wait_ = FALSE)
+    app$set_inputs(`wrangle_data-weight` = "weight", wait_ = FALSE)
+    app$set_inputs(`wrangle_data-height` = "height", wait_ = FALSE)
+    app$set_inputs(`wrangle_data-muac` = "muac", wait_ = FALSE)
 
-  ### Get wrangled values ----
-  vals <- app$get_js("
-    $('#wrangle_data-wrangled thead th').map(function() {
-      return $(this).text();
-    }).get();
-  ") |> as.character()
+    ### Click wrangle button ----
+    app$click(input = "wrangle_data-apply_wrangle")
+    app$wait_for_value(output = "wrangle_data-wrangled", timeout = 40000)
 
-  ### Test check ----
-  testthat::expect_true(all(vals %in% c(
-    "wfhz", "flag_wfhz", "age_days", "mfaz", "flag_mfaz")
-  ))
-      testthat::expect_true(app$get_js("$('#ipc_check-checked').length > 0"))
+    app$wait_for_idle(timeout = 15000)
 
-  #### Stop the app ----
-  app$stop()
+    testthat::expect_true(app$get_js("$('#wrangle_data-wrangled').length > 0"))
+
+    #### Stop the app ----
+    app$stop()
   }
-
 )
-
-
