a1d13aa7b07888f05945d197b6f1b7f62d828690
tomaszaba <tomas.zaba@outlook.com>
2026-01-27 18:00:41 +0200

    Fixes #48

--- R/prev-wasting-screening.R
+++ R/prev-wasting-screening.R
@@ -89,15 +89,15 @@ get_estimates <- function(df, muac, oedema = NULL, raw_muac = FALSE, ...) {
 #' the point prevalence and the counts of positive cases, without necessarily
 #' estimating the uncertainty. This function serves this purpose.
 #'
-#' It first evaluates the quality of the data to determine the appropriate 
-#' prevalence-analysis flow to be employed. Quality is evaluated by estimating 
-#' the observed proportion of children aged 24-59 months of the total children in 
-#' the dataset, then it estimates the p-value of the difference between the 
-#' above-mentioned category against the expected (0.66) and rates it. 
-#' 
-#' If age ratio test is "problematic" and the proportion of children aged 24-59 
-#' months is < 0.66, age-weighting approach is applied to prevalence estimation, 
-#' to account for the over-representation of younger children in the sample; 
+#' It first evaluates the quality of the data to determine the appropriate
+#' prevalence-analysis flow to be employed. Quality is evaluated by estimating
+#' the observed proportion of children aged 24-59 months of the total children in
+#' the dataset, then it estimates the p-value of the difference between the
+#' above-mentioned category against the expected (0.66) and rates it.
+#'
+#' If age ratio test is "problematic" and the proportion of children aged 24-59
+#' months is < 0.66, age-weighting approach is applied to prevalence estimation,
+#' to account for the over-representation of younger children in the sample;
 #' otherwise, a non-age-weighted prevalence is estimated.
 #'
 #' @details
@@ -280,82 +280,43 @@ mw_estimate_prevalence_screening2 <- function(
   if (length(.by) > 0) df <- dplyr::group_by(df, !!!.by)
 
   ## Determine the analysis path that fits the data ----
-  path <- df |>
+  x <- df |>
     dplyr::summarise(
-      age_ratio = rate_agesex_ratio(
-        mw_stattest_ageratio2(
-          {{ age_cat }}, 0.66
-        )$p
-      ),
-      std = rate_std(
-        stats::sd(
-          remove_flags(
-            as.numeric(.data$muac),
-            .from = "raw_muac"
-          ),
-          na.rm = TRUE
-        ),
-        .of = "raw_muac"
-      ),
-      analysis_approach = set_analysis_path(
-        ageratio_class = .data$age_ratio,
-        sd_class = .data$std
+      age_ratio_prop = mw_stattest_ageratio2({{ age_cat }}, 0.66)$observedP,
+      age_ratio_pval = rate_agesex_ratio(
+        mw_stattest_ageratio2({{ age_cat }}, 0.66)$p
       ),
       .groups = "drop"
     )
-
   ## Loop over groups ----
-  for (i in seq_len(nrow(path))) {
+  for (i in seq_len(nrow(x))) {
     if (length(.by) > 0) {
-      vals <- purrr::map(.by, ~ dplyr::pull(path, !!.x)[i])
+      vals <- purrr::map(.by, ~ dplyr::pull(x, !!.x)[i])
       exprs <- purrr::map2(.by, vals, ~ rlang::expr(!!rlang::get_expr(.x) == !!.y))
       data_subset <- dplyr::filter(df, !!!exprs)
     } else {
       data_subset <- df
     }
 
-    analysis_approach <- path$analysis_approach[i]
-
-    if (analysis_approach %in% c("unweighted", "missing")) {
-      if (length(.by) > 0) {
-        r <- get_estimates(
-          df = data_subset,
-          muac = {{ muac }},
-          oedema = {{ oedema }},
-          raw_muac = TRUE,
-          !!!.by
-        )
-      } else {
-        r <- get_estimates(
-          df = data_subset,
-          muac = {{ muac }},
-          oedema = {{ oedema }},
-          raw_muac = TRUE
-        )
-      }
+    if (x$age_ratio_pval[i] == "Problematic" && x$age_ratio_prop[i] < 0.66) {
+      r <- mw_estimate_age_weighted_prev_muac(
+        data_subset,
+        muac = {{ muac }},
+        has_age = FALSE,
+        age_cat = {{ age_cat }},
+        oedema = {{ oedema }},
+        raw_muac = TRUE,
+        !!!.by
+      ) |>
+        dplyr::select(!!!.by, .data$sam_p, .data$mam_p, .data$gam_p)
     } else {
-      if (length(.by) > 0) {
-        r <- mw_estimate_age_weighted_prev_muac(
-          data_subset,
-          muac = muac,
-          has_age = FALSE,
-          age_cat = age_cat,
-          oedema = oedema,
-          raw_muac = TRUE,
-          !!!.by
-        ) |>
-          dplyr::select(!!!.by, .data$sam_p, .data$mam_p, .data$gam_p)
-      } else {
-        r <- mw_estimate_age_weighted_prev_muac(
-          df = data_subset,
-          has_age = FALSE,
-          muac = muac,
-          oedema = oedema,
-          age_cat = age_cat,
-          raw_muac = TRUE
-        ) |>
-          dplyr::select(.data$sam_p, .data$mam_p, .data$gam_p)
-      }
+      r <- get_estimates(
+        df = data_subset,
+        muac = {{ muac }},
+        oedema = {{ oedema }},
+        raw_muac = TRUE,
+        !!!.by
+      )
     }
     results[[i]] <- r
   }
